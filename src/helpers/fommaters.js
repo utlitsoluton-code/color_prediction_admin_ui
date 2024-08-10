4c421061c93e92f70066c6a76204a5114fdfacd2
tanuutl <tanuutlit@gmail.com>
2024-08-10 16:43:05 +0530

    updated

--- src/helpers/fommaters.js
+++
@@ -1,6 +0,0 @@
-// export function TimeFormat(date) {
-//       const time = new Date(value).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-  
-//     return time;
-//   }
-
